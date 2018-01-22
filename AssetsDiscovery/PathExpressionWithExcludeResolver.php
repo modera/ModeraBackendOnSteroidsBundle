68b6813aacf6a25b0e90f288a5f5dacaa5610ecc
LiSinCin <crazy.crazy.eco@gmail.com>
2018-01-22 18:56:47 +0200

    [CLS-1098] adding exlude bundle assets discovery class

---
+++ AssetsDiscovery/PathExpressionWithExcludeResolver.php
@@ -0,0 +1,104 @@
+<?php
+
+namespace Modera\BackendOnSteroidsBundle\AssetsDiscovery;
+
+use Symfony\Component\HttpKernel\Bundle\BundleInterface;
+use Symfony\Component\HttpKernel\KernelInterface;
+
+/**
+ * Same as in PathExpressionResolver but, in case 'excluded_namespaces' parameter is present all bundles name fill be
+ * filtered by it. "@Foo.*Bundle", excluded "@FoxXBundle", "FoxYBundle" will be returned
+ *
+ * @author    Alex Plaksin <alex.plaksin@modera.net>
+ * @copyright 2018 Modera Foundation
+ */
+class PathExpressionWithExcludeResolver
+{
+    /**
+     * @var KernelInterface
+     */
+    private $kernel;
+
+    /**
+     * @param KernelInterface $kernel
+     */
+    public function __construct(KernelInterface $kernel)
+    {
+        $this->kernel = $kernel;
+    }
+
+    /**
+     * @param string $pathExpression
+     *
+     * @return string
+     */
+    public function resolve($pathExpression)
+    {
+        if (substr($pathExpression, 0, 1) == '@') { // bundle's presence indicated as "@" in path
+            $bundleName = null;
+
+            $separatorIndex = strpos($pathExpression, '/');
+            // 1 as start index because we don't need @
+            if (false !== $separatorIndex) {
+                $bundleName = substr($pathExpression, 1, $separatorIndex - 1);
+            } else { // the whole path is just a bundle name
+                $bundleName = substr($pathExpression, 1);
+            }
+
+            // @ModeraBackend.*Bundle/Resources/public/js
+            // ModeraBackend.*Bundle
+
+            /* @var BundleInterface[] $matchedBundles */
+            $matchedBundles = [];
+
+            foreach ($this->kernel->getBundles() as $bundle) {
+                /* @var BundleInterface $bundle */
+
+                $regex = '|^'.$bundleName.'$|';
+
+                if (array_search('@'.$bundle->getName(), $this->getExcludedNamespaces()) !== false) {
+                    continue;
+                }
+
+                if (preg_match($regex, $bundle->getName())) {
+                    $matchedBundles[] = $bundle;
+                }
+            }
+
+            $resolvedPaths = [];
+
+            foreach ($matchedBundles as $matchedBundle) {
+                if (false !== $separatorIndex) {
+                    // @ModeraBackend.*Bundle/Resources/public/js -->
+                    // /var/www/mymegaproject/PathOfBackendBlahBundle/Resources/public/js
+                    $resolvedPaths[] = implode('', [
+                        $matchedBundle->getPath(),
+                        substr($pathExpression, $separatorIndex),
+                    ]);
+                } else { // the whole bundle
+                    $resolvedPaths[] = $matchedBundle->getPath();
+                }
+            }
+
+            return $resolvedPaths;
+        }
+
+        return [$pathExpression];
+    }
+
+    /**
+     * @return array
+     */
+    private function getExcludedNamespaces()
+    {
+        if (!$this->kernel->getContainer()->hasParameter('modera_backend_on_steroids.config') ) return [];
+
+        $config = $this->kernel->getContainer()->getParameter('modera_backend_on_steroids.config');
+
+        if (!is_array($config)) return [];
+
+        if (empty($config['compiler']['excluded_namespaces'])) return [];
+
+        return $config['compiler']['excluded_namespaces'];
+    }
+}
---
+++ Tests/Unit/AssetsDiscovery/PathExpressionWithExcludeResolverTest.php
@@ -0,0 +1,87 @@
+<?php
+
+namespace Modera\BackendOnSteroidsBundle\Tests\Unit\AssetsDiscovery;
+
+use Modera\BackendOnSteroidsBundle\AssetsDiscovery\PathExpressionWithExcludeResolver;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+
+/**
+ * @author    Alex Plaksin <alex.plaksin@modera.net>
+ * @copyright 2018 Modera Foundation
+ */
+class PathExpressionResolverWithExcludeTest extends \PHPUnit_Framework_TestCase
+{
+    private function createBundle($name, $path)
+    {
+        $bundle = \Phake::mock('Symfony\Component\HttpKernel\Bundle\BundleInterface');
+        \Phake::when($bundle)->getName()->thenReturn($name);
+        \Phake::when($bundle)->getPath()->thenReturn($path);
+
+        return $bundle;
+    }
+
+    public function testResolve()
+    {
+        $kernel = \Phake::mock('Symfony\Component\HttpKernel\KernelInterface');
+        \Phake::when($kernel)->getBundles()->thenReturn([
+            $this->createBundle('ModeraBackendFooBundle', '/var/www/myapp/vendor/modera/backend-foo'),
+            $this->createBundle('ModeraBackenFooBundle', '/var/www/myapp/vendor/modera/backen-foo'), // nein
+            $this->createBundle('PartnerBackendFooBundle', '/var/www/myapp/vendor/partner/backend-foo'),
+        ]);
+
+
+        $container = \Phake::mock(ContainerInterface::class);
+
+        $this->assertTrue(!$container->hasParameter('modera_backend_on_steroids.config'));
+
+        \Phake::when($kernel)->getContainer()->thenReturn($container);
+
+        $resolver = new PathExpressionWithExcludeResolver($kernel);
+
+        $paths = $resolver->resolve('@ModeraBackend.*Bundle/Resources/public/js');
+
+        $this->assertEquals(1, count($paths));
+        $this->assertEquals('/var/www/myapp/vendor/modera/backend-foo/Resources/public/js', $paths[0]);
+
+        $paths = $resolver->resolve('@PartnerBackend.*Bundle/Resources/public/js/runtime');
+
+        $this->assertEquals(1, count($paths));
+        $this->assertEquals('/var/www/myapp/vendor/partner/backend-foo/Resources/public/js/runtime', $paths[0]);
+    }
+
+
+    public function testExcludedDirPresentInResolver()
+    {
+        $kernel = \Phake::mock('Symfony\Component\HttpKernel\KernelInterface');
+        \Phake::when($kernel)->getBundles()->thenReturn([
+            $this->createBundle('ModeraBackendXFooBundle', '/var/www/myapp/vendor/modera/backend-x-foo'),
+            $this->createBundle('ModeraBackendYFooBundle', '/var/www/myapp/vendor/modera/backend-y-foo'), // both matched but exclude should remove this one
+            $this->createBundle('PartnerBackendFooBundle', '/var/www/myapp/vendor/partner/backend-foo'),
+        ]);
+
+        $config =
+            array('compiler'=>
+                array('excluded_namespaces'=>['@ModeraBackendYFooBundle'])
+            )
+        ;
+        $container = \Phake::mock(ContainerInterface::class);
+        \Phake::when($container)->getParameter('modera_backend_on_steroids.config')->thenReturn($config);
+        \Phake::when($container)->hasParameter('modera_backend_on_steroids.config')->thenReturn(true);
+
+        $this->assertEquals($config, $container->getParameter('modera_backend_on_steroids.config'));
+
+        \Phake::when($kernel)->getContainer()->thenReturn($container);
+
+        $resolver = new PathExpressionWithExcludeResolver($kernel);
+
+        $paths = $resolver->resolve('@ModeraBackend.*Bundle/Resources/public/js');
+
+        $this->assertEquals(1, count($paths));
+        $this->assertEquals('/var/www/myapp/vendor/modera/backend-x-foo/Resources/public/js', $paths[0]);
+
+        $paths = $resolver->resolve('@PartnerBackend.*Bundle/Resources/public/js/runtime');
+
+        $this->assertEquals(1, count($paths));
+        $this->assertEquals('/var/www/myapp/vendor/partner/backend-foo/Resources/public/js/runtime', $paths[0]);
+    }
+}
